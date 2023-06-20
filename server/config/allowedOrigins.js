7ef40168ade7f0d5977f50538464cd09d7a45568
codewithkhalil <dareythefirst@gmail.com>
2023-06-20 23:41:41 +0100

    fixes

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:3001',
-    'https://themarketplace.onrender.com/'
+    'https://themarketplace.onrender.com'
 ]
 
 module.exports = allowedOrigins
