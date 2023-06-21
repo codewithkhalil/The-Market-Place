1e1b27d52658525b4e0ee8ec825e9b2734caf9e7
codewithkhalil <dareythefirst@gmail.com>
2023-06-21 09:47:05 +0100

    fixes

--- server/server.js
+++ server/server.js
@@ -39,9 +39,9 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
